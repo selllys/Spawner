3d1cec36549734a61e3fc33a81eb3c8b36e9e4ba
Andrew Sellys <a.selllys@gmail.com>
2021-11-18 17:25:37 +0500

    update EnemiesSpawner

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -1,40 +1,38 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour
 {
-    [SerializeField, Min(0)]
-    private float _startTime = 3f;
-
-    [SerializeField, Min(0)]
-    private float _spawnDelay = 2f;
+    [SerializeField, Min(0)] private float _spawnDelay = 2f;
 
     private List<SpawnPoint> _spawnPoints;
-    private Queue<SpawnPoint> _spawnQueue;
 
     private void Start()
     {
         _spawnPoints = GetComponentsInChildren<SpawnPoint>().ToList();
-        _spawnQueue = new Queue<SpawnPoint>();
 
-        InvokeRepeating(nameof(SpawnEnemy), _startTime, _spawnDelay);
-    }
-
-    private void SpawnEnemy()
-    {
-        if (_spawnPoints.Count == 0)
+        if (_spawnPoints.Count > 0)
+        {
+            StartCoroutine(SpawnEnemies());
+        }
+        else
         {
             Debug.LogError("Add spawn points!");
-            return;
         }
+    }
 
-        if (_spawnQueue.Count == 0)
+    private IEnumerator SpawnEnemies()
+    {
+        var delay = new WaitForSeconds(_spawnDelay);
+
+        foreach (SpawnPoint nextSpawnPoint in _spawnPoints)
         {
-            _spawnQueue = new Queue<SpawnPoint>(_spawnPoints);
+            yield return delay;
+            nextSpawnPoint.SpawnEnemy();
         }
 
-        SpawnPoint nextSpawnPoint = _spawnQueue.Dequeue();
-        nextSpawnPoint.SpawnEnemy();
+        yield return SpawnEnemies();
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,7 @@ public class Enemy : MonoBehaviour
 {
     private const float Lifespan = 10f;
 
-    [SerializeField, Min(0.1f)]
-    private float _movementSpeed = 1f;
+    [SerializeField, Min(0.1f)] private float _movementSpeed = 1f;
 
     private void Start()
     {
